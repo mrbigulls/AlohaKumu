98e0f0c8df5335c64e4cc2248e32bd84936d9487
Spencer Langley <mrbigulls@hotmail.com>
2012-12-03 16:54:10 -0600

    0.1 Deployment Test
    Try removing ignore setting for bin directory

---
+++ TestApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "This is a test to make GitHub and AppHarbor working together.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "I don't know why it hasn't been working so far.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Please, for the love of God, just deploy all the references with the project.";
+
+            return View();
+        }
+    }
+}
